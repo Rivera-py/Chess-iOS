2d94dd72e3d27b9381fc129d68ddc6ae80aeb60f
J-Riv-Py <Rivera-py@users.noreply.github.com>
2021-06-26 09:02:40 +0100

    Creating a new board class
    Planning to manage board operations in its own class.

---
+++ Chess-iOS/board.py
@@ -0,0 +1,11 @@
+"""Object for handling all board operations and data."""
+
+class Board:
+	"""Game board object."""
+	
+	def __init__(self):
+		"""Construct class."""
+		
+		self.is_select = False
+	
+	def turn()
